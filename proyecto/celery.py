a37d566ec847411b8f45b2504839fc4cd7c384b9
daniel <danilacasito8@gmail.com>
2026-03-10 13:06:21 +0100

    Add Celery configuration and update requirements.txt

---
+++ proyecto/celery.py
@@ -0,0 +1,10 @@
+from celery import Celery
+import os
+
+app = Celery('proyecto')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto.settings')
+app.config_from_object('django.conf:settings', namespace="CELERY")
+
+app.autodiscover_tasks()
+
+from tienda import tasks as tienda_tasks
