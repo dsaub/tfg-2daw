747194b50757aea614e98bc6fcf05f88cb60d95b
Daniel <danilacasito8@gmail.com>
2026-04-22 09:15:24 +0200

    feat: add API URL routing to urlpatterns

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -19,11 +19,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from tienda import views as tienda_views
+from tienda.api import api
 
 urlpatterns = [
     path('', tienda_views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('tienda/', include('tienda.urls'))
+    path('tienda/', include('tienda.urls')),
+    path('api/', api.urls)
 ]
 
 if settings.DEBUG:
