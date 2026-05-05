c9447edfc76efdc9565dd89db6c3a24ad71fb625
Daniel <danilacasito8@gmail.com>
2026-05-05 12:45:54 +0200

    Fix Celery: restore autodiscover_tasks to default behavior

--- proyecto/celery.py
+++ proyecto/celery.py
@@ -5,4 +5,4 @@ app = Celery('proyecto')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto.settings')
 app.config_from_object('django.conf:settings', namespace="CELERY")
 
-app.autodiscover_tasks(['tienda.tasks'])
+app.autodiscover_tasks()
