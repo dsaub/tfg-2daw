fed5b5847931b8497780e8c5be5029b5a0e33e72
Daniel <danilacasito8@gmail.com>
2026-05-26 09:44:53 +0200

    fix: update SECRET_KEY to use environment variable instead of hardcoded value

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -78,7 +78,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-#g((q@lvnkt(j6)2(gvtn0px)r2r(911)pv59i(6w)5e!_-^ao')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_bool('DEBUG', True)
