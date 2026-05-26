503b653877c0e43c0ce7bb708dbe9482eee800ce
Daniel <danilacasito8@gmail.com>
2026-05-26 13:21:03 +0200

    refactor: clean up send_email function and remove outdated SMTP implementation

--- tienda/utilities.py
+++ tienda/utilities.py
@@ -4,30 +4,6 @@ from django.conf import settings
 
 
 logger = logging.getLogger("email.system")
-#
-#def send_email(dest: str, title: str, body: str):
-#    context = ssl.create_default_context()
-#    try:
-#        with smtplib.SMTP(settings.SMTP_ENDPOINT, settings.SMTP_PORT) as server:
-#
-#        
-#            server.ehlo()
-#            server.starttls(context=context)
-#            server.ehlo()
-#            server.login(settings.SMTP_USERNAME, settings.SMTP_PASSWORD)
-#        
-#            message = """\
-#Subject: {}
-#{}
-#            """.format(title, body)
-#            server.sendmail(settings.SMTP_EMAIL, dest, message)
-#            logger.info("EMAIL_SENT to=%s subject=%s", dest, title)
-#        
-#    except Exception as e:
-#        logger.exception("EMAIL_SEND_FAILED to=%s subject=%s error=%s", dest, title, str(e))
-#        return (False, e)
-#    
-#    return (True,)
 
 def send_email(dest: str, title: str, body: str):
     try:
@@ -40,7 +16,7 @@ def send_email(dest: str, title: str, body: str):
         )
 
         logger.info("EMAIL_SENT to=%s subject=%s", dest, title)
-        return (True,)
+        return (True, None)
     except Exception as e:
         logger.exception("EMAIL_SEND_FAILED to=%s subject=%s error=%s", dest, title, str(e))
         return (False, e)
